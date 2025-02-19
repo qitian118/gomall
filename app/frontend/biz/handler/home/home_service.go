839efa705e9f3bad11b066c4675ed2d1ad17d8a3
qitian118 <190928106qitian118@users.noreply.github.com>
2025-02-19 19:36:49 +0800

    前端课程部分完成

--- app/frontend/biz/handler/home/home_service.go
+++ app/frontend/biz/handler/home/home_service.go
@@ -21,8 +21,7 @@ func Home(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 
-	resp := &home.Empty{}
-	resp, err = service.NewHomeService(ctx, c).Run(&req)
+	resp, err := service.NewHomeService(ctx, c).Run(&req)
 	if err != nil {
 		utils.SendErrResponse(ctx, c, consts.StatusOK, err)
 		return
--- app/frontend/biz/service/home.go
+++ app/frontend/biz/service/home.go
@@ -16,11 +16,18 @@ func NewHomeService(Context context.Context, RequestContext *app.RequestContext)
 	return &HomeService{RequestContext: RequestContext, Context: Context}
 }
 
-func (h *HomeService) Run(req *home.Empty) (resp *home.Empty, err error) {
-	//defer func() {
-	// hlog.CtxInfof(h.Context, "req = %+v", req)
-	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
-	//}()
-	// todo edit your code
-	return
+func (h *HomeService) Run(req *home.Empty) (map[string]any, error) {
+	resp := make(map[string]any)
+
+	items := []map[string]any{
+		{"Name": "t-shirt1", "Price": 100, "Picture": "/static/image/t-shirt1.png"},
+		{"Name": "t-shirt2", "Price": 101, "Picture": "/static/image/t-shirt2.png"},
+		{"Name": "t-shirt3", "Price": 102, "Picture": "/static/image/t-shirt3.png"},
+		{"Name": "t-shirt4", "Price": 103, "Picture": "/static/image/t-shirt4.png"},
+		{"Name": "t-shirt5", "Price": 104, "Picture": "/static/image/t-shirt5.png"},
+		{"Name": "t-shirt6", "Price": 105, "Picture": "/static/image/t-shirt6.png"},
+	}
+	resp["Title"] = "Hot Sales"
+	resp["Items"] = items
+	return resp, nil
 }
