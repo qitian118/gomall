ddebc7a59b046cac0ac62aa5e33c7a20b2b67ca0
qitian118 <190928106qitian118@users.noreply.github.com>
2025-02-24 15:37:36 +0800

    15:37 该死的报错终于解决了，2月24日

--- app/user/biz/dal/mysql/init.go
+++ app/user/biz/dal/mysql/init.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"github.com/cloudwego/biz-demo/gomall/app/user/biz/model"
-	"github.com/cloudwego/biz-demo/gomall/app/user/conf"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -17,7 +16,7 @@ var (
 )
 
 func Init() {
-	dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/user?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
 	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
--- app/user/biz/service/register_test.go
+++ app/user/biz/service/register_test.go
@@ -2,14 +2,19 @@ package service
 
 import (
 	"context"
-	"reflect"
 	"testing"
 
-	"github.com/cloudwego/biz-demo/gomall/app/frontend/biz/dal/mysql"
+	"github.com/cloudwego/biz-demo/gomall/app/user/biz/dal/mysql"
 	user "github.com/cloudwego/biz-demo/gomall/rpc_gen/kitex_gen/user"
+	"github.com/joho/godotenv"
 )
 
 func TestRegister_Run(t *testing.T) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		t.Logf("load env failed")
+	}
+
 	mysql.Init()
 	ctx := context.Background()
 	s := NewRegisterService(ctx)
@@ -23,29 +28,29 @@ func TestRegister_Run(t *testing.T) {
 	// todo: edit your unit test
 }
 
-func TestRegisterService_Run(t *testing.T) {
-	type args struct {
-		req *user.RegisterReq
-	}
-	tests := []struct {
-		name     string
-		s        *RegisterService
-		args     args
-		wantResp *user.RegisterResp
-		wantErr  bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotResp, err := tt.s.Run(tt.args.req)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("RegisterService.Run() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(gotResp, tt.wantResp) {
-				t.Errorf("RegisterService.Run() = %v, want %v", gotResp, tt.wantResp)
-			}
-		})
-	}
-}
+// func TestRegisterService_Run(t *testing.T) {
+// 	type args struct {
+// 		req *user.RegisterReq
+// 	}
+// 	tests := []struct {
+// 		name     string
+// 		s        *RegisterService
+// 		args     args
+// 		wantResp *user.RegisterResp
+// 		wantErr  bool
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			gotResp, err := tt.s.Run(tt.args.req)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("RegisterService.Run() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(gotResp, tt.wantResp) {
+// 				t.Errorf("RegisterService.Run() = %v, want %v", gotResp, tt.wantResp)
+// 			}
+// 		})
+// 	}
+// }
