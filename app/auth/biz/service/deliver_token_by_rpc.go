1d62657c8cae432a1ff55c9ced9a0d78505c7cfe
yibajianghudao <w2027075535@outlook.com>
2025-03-03 03:37:38 +0800

    实现接口

--- app/auth/biz/service/deliver_token_by_rpc.go
+++ app/auth/biz/service/deliver_token_by_rpc.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"context"
+	"time"
 
 	auth "github.com/cloudwego/biz-demo/gomall/rpc_gen/kitex_gen/auth"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type DeliverTokenByRPCService struct {
@@ -13,9 +15,38 @@ func NewDeliverTokenByRPCService(ctx context.Context) *DeliverTokenByRPCService 
 	return &DeliverTokenByRPCService{ctx: ctx}
 }
 
+type CustomClaims struct {
+	UserId   int32  `json:"user_id"`
+	UserRole string `json:"user_role"`
+	Resource string `json:"resource"`
+	Action   string `json:"action"`
+	jwt.RegisteredClaims
+}
+
+var secretKey = []byte("your-256-bit-secret")
+
 // Run create note info
 func (s *DeliverTokenByRPCService) Run(req *auth.DeliverTokenReq) (resp *auth.DeliveryResp, err error) {
 	// Finish your business logic.
+	// Finish your business logic.
+	//这里中间件会放行分发操作
 
-	return
+	claims := CustomClaims{
+		UserId: req.UserId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			Issuer:    "your-app-name",
+		},
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := jwtToken.SignedString(secretKey)
+	if err != nil {
+		return nil, err
+	}
+	resp = &auth.DeliveryResp{
+		Token: token,
+	}
+
+	return resp, nil
 }
