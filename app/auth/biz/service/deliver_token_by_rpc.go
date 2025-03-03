4c1d3c3f3664fe3cc110c9bf12dd8916780d7e2a
qitian118 <190928106qitian118@users.noreply.github.com>
2025-03-03 14:16:03 +0800

    deliver_token_by_rpc

--- app/auth/biz/service/deliver_token_by_rpc.go
+++ app/auth/biz/service/deliver_token_by_rpc.go
@@ -2,9 +2,23 @@ package service
 
 import (
 	"context"
+	"time"
 
 	auth "github.com/cloudwego/biz-demo/gomall/rpc_gen/kitex_gen/auth"
+	"github.com/golang-jwt/jwt/v5"
 )
+
+// 自定义声明结构
+type CustomClaims struct {
+	UserId   int32  `json:"user_id"`
+	UserRole string `json:"user_role"`
+	Resource string `json:"resource"`
+	Action   string `json:"action"`
+	jwt.RegisteredClaims
+}
+
+// todo 暂时写死
+var secretKey = []byte("your-256-bit-secret")
 
 type DeliverTokenByRPCService struct {
 	ctx context.Context
@@ -16,6 +30,28 @@ func NewDeliverTokenByRPCService(ctx context.Context) *DeliverTokenByRPCService 
 // Run create note info
 func (s *DeliverTokenByRPCService) Run(req *auth.DeliverTokenReq) (resp *auth.DeliveryResp, err error) {
 	// Finish your business logic.
+	// 这里中间件会放行分发操作
 
-	return
+	claims := CustomClaims{
+		UserId:   req.UserId,
+		UserRole: req.UserRole,
+		Resource: req.Resource,
+		Action:   req.Action,
+
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			Issuer:    "your-app-name",
+		},
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := jwtToken.SignedString(secretKey)
+	if err != nil {
+		return nil, err
+	}
+	resp = &auth.DeliveryResp{
+		Token: token,
+	}
+
+	return resp, nil
 }
