17655143bdd69f3b9cb80227b6f8cb7d0b922e4c
yibajianghudao <w2027075535@outlook.com>
2025-03-05 23:43:22 +0800

    删除调试代码

--- app/frontend/biz/utils/userinfo.go
+++ app/frontend/biz/utils/userinfo.go
@@ -13,9 +13,9 @@ import (
 func UserInfo(ctx context.Context, c *app.RequestContext, content map[string]any) map[string]any {
 	userId := frontendUtils.GetUserIdFromCtx(ctx)
 	content["user_id"] = userId
-	fmt.Println("berfore userclient")
+	// fmt.Println("berfore userclient")
 	resp, err := rpc.UserClient.UserInfo(ctx, &user.UserInfoReq{UserId: userId})
-	fmt.Println("after userclient")
+	// fmt.Println("after userclient")
 	if err != nil {
 		fmt.Println("have error")
 		fmt.Println(err)
